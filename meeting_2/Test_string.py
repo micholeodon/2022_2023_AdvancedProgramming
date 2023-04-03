a8633a6093f4cdbba98786fd520477f1a8009982
micholeodon <michu.kom@gmail.com>
2023-04-03 14:59:41 +0200

    meeting 2 files

---
+++ meeting_2/Test_string.py
@@ -0,0 +1,14 @@
+print('Hello "World!" ') 
+imie = input('Write your name: ')
+print(imie)
+
+surname = input('Write your surname: ')
+
+surname.upper()
+# surname = surname.upper()
+imie = imie.lower()
+s = imie + ' ' + surname
+print(s)
+
+print("Ala ma \nkota!")
+
---
+++ meeting_2/variables.py
@@ -0,0 +1,48 @@
+ageOfDog = 4 # integer
+pi = 3.1415 # floating point number
+name = 'Michał'
+z = complex(4,5)
+
+# boolean
+# result = True
+# isError = False
+
+# # lists, tuples, sets
+# listOfSth = []
+# anotherList = [1, 'sd', -3, 3.45, complex(2,3), -3, -3, -3, [3,4,3]]
+
+# # tuples cannot be changed
+# emptyTuple = ()
+# anotherTuple = ('sd',24324324,2.334232)
+# pointCoordinate = (-4,5)
+
+# # set is not multiset - cannot contain repetitions
+# someSet = {2,4,5,5}
+# print(someSet)
+
+# # dictionary
+# d = {
+#     1 : 2,
+#     2 : 'sdhskw',
+#     'doggo' : 'hau!' 
+# }
+# print(d[2])
+
+
+exampleList = ['ok', 'coffee', 51, 'water', 'computer', 'doggo']
+print(exampleList[0])
+print(exampleList[2])
+print(exampleList[-1])
+print(exampleList[-2])
+
+print(exampleList[0:2]) # [2] not included 
+
+# for i in range(2,7):
+#     print(i, end='#@*DSHJ#')
+
+x = 5
+y = -3.2
+print(f"Your point has coordinates ({x},{y})")
+print("Your point has coordinates ({},{})".format(x,y))
+print("Your point has coordinates (" + str(x) + ',' + str(y) +')')
+
